ac6d07417fd91cd11881d2ca788875b83fbfe3a5
Sobirjon005 <sobirjon0305@gmail.com>
2024-12-15 20:12:21 +0500

    Get Method | Fast Api

--- 01-books/books.py
+++ 01-books/books.py
@@ -17,7 +17,27 @@ async def read_all_books():
     return BOOKS
 
 
-@app.get("/books/{dynamic_param")
-async def read_book(dynamic_param):
-    return BOOKS[dynamic_param]
+@app.get("/books/{book_title}")
+async def read_book(book_title):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
 
+
+@app.get("/books/")
+async def read_book_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/{author}/")
+async def read_author_category_by_query(author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
