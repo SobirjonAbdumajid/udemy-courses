9b75d2fa0ffa936857b100ba2c5f07dbb8ae80ac
Sobirjon005 <sobirjon0305@gmail.com>
2024-12-25 08:31:11 +0500

    Fast API

--- 02-todo-app/TodoApp/routers/auth.py
+++ 02-todo-app/TodoApp/routers/auth.py
@@ -1,3 +1,4 @@
+from http.client import HTTPException
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
@@ -34,6 +35,15 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def get_user(db: db_dependency,
                    user_request: MakeUserRequest):
@@ -48,3 +58,12 @@ async def get_user(db: db_dependency,
     )
     db.add(user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed authentication"
+    return "Successfully logged in"
