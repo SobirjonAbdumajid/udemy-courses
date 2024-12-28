18fb32177fb23bd4a75b7bcc3decfde83ba752e9
Sobirjon005 <sobirjon0305@gmail.com>
2024-12-28 17:15:28 +0500

    Fast API

--- 02-todo-app/TodoApp/routers/todos.py
+++ 02-todo-app/TodoApp/routers/todos.py
@@ -28,6 +28,7 @@ class TodoRequest(BaseModel):
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+
 @router.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
