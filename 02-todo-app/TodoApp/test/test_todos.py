2cea466df786e064414cd032bff5b57dd36377f2
Sobirjon005 <sobirjon0305@gmail.com>
2025-01-29 18:23:01 +0500

    tasks

--- 02-todo-app/TodoApp/test/test_todos.py
+++ 02-todo-app/TodoApp/test/test_todos.py
@@ -70,3 +70,41 @@ def test_read_all_authenticated(test_todo):
             "owner_id": 1
         }
     ]
+
+
+def test_read_one_authenticated(test_todo):
+    response = client.get('/todo/1')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        'id': 1,
+        "title": "Learn code",
+        "description": "don't give up",
+        "priority": 5,
+        "complete": False,
+        "owner_id": 1
+    }
+
+
+def test_read_todo_authenticated_not_found():
+    response = client.get('/todo/999')  # Test a non-existent ID
+    assert response.json()['status_code'] == 404
+    assert response.json() == {'detail': 'Todo not found', 'headers': None, 'status_code': 404}
+
+
+def test_create_todo(test_todo):
+    request_data={
+        'title': 'New Todo!',
+        'description':'New todo description',
+        'priority': 5,
+        'complete': False,
+    }
+
+    response = client.post('/todo/', json=request_data)
+    assert response.status_code == 201
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 2).first()
+    assert model.title == request_data.get('title')
+    assert model.description == request_data.get('description')
+    assert model.priority == request_data.get('priority')
+    assert model.complete == request_data.get('complete')
