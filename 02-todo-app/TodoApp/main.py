b4c4a277997da0cad4f88e997dc695d2f3a4de3d
Sobirjon005 <sobirjon0305@gmail.com>
2025-02-02 06:25:18 +0500

    tasks

--- 02-todo-app/TodoApp/main.py
+++ 02-todo-app/TodoApp/main.py
@@ -3,9 +3,21 @@ from .database import engine
 from .models import Base
 from .routers import auth, todos, admin, users
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+
+templates = Jinja2Templates(directory="TodoApp/templates")
+
+
+app.mount("/static", StaticFiles(directory="TodoApp/static"), name="static")
+
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
 @app.get('/health/')
